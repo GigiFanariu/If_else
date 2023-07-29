731bc30134d9a30815de9909ea6b8b744e4fd068
GigiFanariu <139012939+GigiFanariu@users.noreply.github.com>
2023-07-29 11:43:23 +0200

    completato

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        String myName = "Gigi";
+        if (myName.length() % 2 == 0)
+            System.out.println("The number of letters in my name is even.");
+        else
+            System.out.println("The number of letters in my name is odd.");
+
+
+
+    }
+}
